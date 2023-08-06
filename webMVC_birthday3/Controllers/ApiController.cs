29af9eba6ef0c23c9db076d0b7102efa3df9a66d
iTamerlan <itamerlan@test.ru>
2023-08-06 12:06:56 +0300

    Добавлена заготовка для API

---
+++ webMVC_birthday3/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBirthdayMVC.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: UserController
+        [HttpGet]
+        public ActionResult users()
+        {
+            return View();
+        }
+
+        /*[HttpGet]
+        public ActionResult users()
+        {
+            return View();
+        }*/
+
+    }
+}
---
+++ webMVC_birthday3/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace WebBirthdayMVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = ""; // имя пользователя
+        [Column(TypeName = "date")]
+        public DateTime Birthday { get; set; } // День рождения пользователя
+        public bool Type { get; set; } // Важное?
+        public string? Photo { get; set; } // Фото пользователя // Convert.FromBase64String (string s);
+        public int DayOfYear
+        {
+            get
+            {
+                int temp = DayOfYear;
+                int n = DateTime.Now.DayOfYear;
+                if (temp < n)
+                {
+                    temp += 366;
+                }
+                return temp;
+            }
+            set
+            {
+                this.DayOfYear = Birthday.DayOfYear;
+            }                
+        }
+        //public decimal DayOfYear => Birthday.DayOfYear;
+        /*[NotMapped]*/
+    }
+}
